3a1146f9fbd35b3421e637826808c07c3411e564
Tijs-B <tijs.bergmans@telenet.be>
2023-02-28 15:28:47 +0100

    Fix zsh autosuggestions not showing up because of the color

--- zsh/plugins_after.zsh
+++ zsh/plugins_after.zsh
@@ -4,4 +4,5 @@ source ~/.zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
 # zsh autosuggestions
 source ~/.zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
+ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=60'
 
