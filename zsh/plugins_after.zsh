179257791bb2cbc1be6aa10e086067d71b7e0407
Tijs-B <tijs.bergmans@telenet.be>
2023-03-14 13:22:24 +0100

    zsh: add better vi mode plugin

--- zsh/plugins_after.zsh
+++ zsh/plugins_after.zsh
@@ -1,8 +1,10 @@
-
 # Syntax highlighting
 source ~/.zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
 # zsh autosuggestions
 source ~/.zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
 ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=60'
+
+# better vi mode
+source ~/.zsh/plugins/zsh-vi-mode/zsh-vi-mode.plugin.zsh
 
