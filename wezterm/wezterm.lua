a749474f97f1f136c08811f2022ecf0f6b06b43c
Tijs-B <14929648+Tijs-B@users.noreply.github.com>
2024-08-13 14:11:14 +0200

    Change wezterm config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,18 +1,16 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
+-- Colors --
 local color_scheme = "OneDark (Gogh)"
 local onedark = wezterm.color.get_builtin_schemes()[color_scheme]
 
 local bg_active = onedark.background
-local fg_active = onedark.foreground
+local fg_active = "#80899b" -- Original foreground: #5c6370
 local bg_inactive = "#15191F"
 local fg_inactive = "#3c414a"
 
 config.color_scheme = color_scheme
-config.font = wezterm.font({ family = "MesloLGLDZ Nerd Font Mono" })
-config.font_size = 12
-
 config.colors = {
 	tab_bar = {
 		background = bg_active,
@@ -33,7 +31,15 @@ config.colors = {
 			fg_color = fg_active,
 		},
 	},
+	-- Orignal foreground: #5c6370
+	foreground = fg_active,
 }
+
+-- Visuals --
+config.font = wezterm.font({ family = "MesloLGLDZ Nerd Font Mono" })
+config.font_size = 12
+config.harfbuzz_features = { "calt=1", "clig=1", "liga=1" }
+config.freetype_load_flags = "NO_HINTING"
 
 config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
 config.window_padding = {
@@ -45,10 +51,33 @@ config.window_padding = {
 config.window_frame = {
 	active_titlebar_bg = bg_inactive,
 }
+config.window_background_opacity = 0.9
+config.macos_window_background_blur = 30
+
 config.enable_scroll_bar = true
 
-config.freetype_load_flags = "NO_HINTING"
+config.initial_rows = 30
+config.initial_cols = 120
 
+-- Behaviour
+config.hide_mouse_cursor_when_typing = false
 config.adjust_window_size_when_changing_font_size = false
+config.use_resize_increments = true
+config.scrollback_lines = 20000000
+
+-- Keys
+config.keys = {
+	-- Option + left/right moves a word left or right
+	{
+		key = "LeftArrow",
+		mods = "OPT",
+		action = wezterm.action.SendString("\x1bb"),
+	},
+	{
+		key = "RightArrow",
+		mods = "OPT",
+		action = wezterm.action.SendString("\x1bf"),
+	},
+}
 
 return config
