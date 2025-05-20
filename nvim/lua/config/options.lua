58d736564579959e0d143197f2b12b088c9f9e36
Tijs-B <14929648+Tijs-B@users.noreply.github.com>
2025-05-20 10:44:14 +0200

    neovim don't share clipboard with system

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,4 +2,5 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.g.python3_host_prog = '/Users/tijs/.virtualenvs/neovim/bin/python'
+vim.g.python3_host_prog = "/Users/tijs/.virtualenvs/neovim/bin/python"
+vim.opt.clipboard = ""
